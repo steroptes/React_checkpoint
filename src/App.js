c72ee03f610f6bed23119bc8e8a6da1531ffc0d7
steroptes <rjeb.sfar.med@gmail.com>
2020-05-14 14:33:33 +0100

    committing first version of the react application web REACT-CP

--- src/App.js
+++ src/App.js
@@ -5,20 +5,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container-fluid">
+        <div class="row no-gutter">
+          <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+          <div class="col-md-8 col-lg-6">
+            <div class="login d-flex align-items-center py-5">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-9 col-lg-8 mx-auto">
+                    <h3 class="login-heading mb-4 font-weight-bold">Welcome! </h3>
+                    <form>
+                      <div class="form-label-group">
+                        <input type="text" id="inputFirstName" class="form-control" placeholder="First Name" required autofocus></input>
+                        <label for="inputFirstName">First Name</label>
+                      </div>
+                      <div class="form-label-group">
+                        <input type="text" id="inputLastName" class="form-control" placeholder="Laste Name" required autofocus></input>
+                        <label for="inputLastName">Last Name</label>
+                      </div>
+                      <div class="form-label-group">
+                        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus></input>
+                          <label for="inputEmail">Email address</label>
+                      </div>
+
+                        <div class="form-label-group">
+                          <input type="password" id="inputPassword" class="form-control" placeholder="Password" required></input>
+                            <label for="inputPassword">Password</label>
+                        </div>
+
+                        <div class="custom-control custom-checkbox mb-3">
+                          <input type="checkbox" class="custom-control-input" id="customCheck1"></input>
+                          <label class="custom-control-label" for="customCheck1">Subscribe to the newsletter</label>
+                        </div>
+                            <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit">Subscribe</button>
+                      </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
